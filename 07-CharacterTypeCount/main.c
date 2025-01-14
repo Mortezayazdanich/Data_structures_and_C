be8ec7d46f5c9482df2b0bbb8b294e98cf96ae11
Mortezayazdanich <56220550+Mortezayazdanich@users.noreply.github.com>
2025-01-14 12:12:25 +0100

    To count different type Chars in a string, Added.

---
+++ 07-CharacterTypeCount/main.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+int isvowel(char c);
+
+#define SIZE  50
+
+int main(void){
+  char str[SIZE];
+  bool pal;
+  int len, vowels = 0, numerics = 0, consonants = 0, spaces = 0;
+  
+  printf("Enter a String(50 Chars):");
+  //scanf("%s",str);
+  fgets(str, SIZE, stdin);
+  printf("\n%s\n",str);
+  len = strlen(str);
+  for(int i = 0; i < len; i++){
+    if (isdigit(str[i])) {
+      numerics += 1;
+    } else if (isvowel(str[i])) {
+      vowels += 1;
+    } else if (str[i] == ' ') {
+      spaces += 1;
+    } else if (!isvowel(str[i])) {
+      consonants += 1;
+    }
+  }
+  
+  printf("The Following are the counts for the string: %s\n", str);
+  printf("    |--> Vowels Count:     %d\n", vowels);
+  printf("    |--> Digits Count:     %d\n", numerics);
+  printf("    |--> Consonants Count: %d\n", consonants);
+  printf("    |--> Spaces Count:     %d\n", spaces);
+  
+  return 0;
+}
+
+int isvowel(char c) {
+    switch(tolower(c)){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            return 1;
+        default:
+            return 0;
+    }
+}
