47e769865880b7a46bff46a7b9811acebae5de20
Mortezayazdanich <56220550+Mortezayazdanich@users.noreply.github.com>
2025-01-14 11:15:39 +0100

    Reverse String without using builtin functions Added.

---
+++ 05-ReverseString/main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SIZE  20 
+#define STR_SIZE  12
+
+int main(void){
+  char str[] = "Hello World!",tmp_c;
+  int k = STR_SIZE-1, tmp;
+  for(int i = 0; i < STR_SIZE; i++){
+    for(int j = 0; j < k; j++){
+        tmp_c = str[j+1];
+        str[j+1] = str[j];
+        str[j] = tmp_c;
+    }
+    k -= 1;
+  }
+
+  for(int y = 0; y < STR_SIZE; y++){
+    printf("%c", str[y]);
+  }
+  return 0;
+}
